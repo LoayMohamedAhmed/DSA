793d9f3550c1d3ab692e6d971067764c9958b3e0
LoayMohamedAhmed <looay432@gmail.com>
2024-09-19 08:11:21 +0300

    queue implementation in python

---
+++ Stack & queue/queue.py
@@ -0,0 +1,38 @@
+class Queue:
+
+    def __init__(self):
+        self.Queue = []
+        self.size =0
+
+    def enqueue(self, element):
+        self.Queue.append(element)
+        self.size+=1
+
+    def is_empty(self):
+        if self.size == 0:
+            return True
+        else:
+            return False
+    def dequeue(self):
+        if not self.is_empty():
+            self.size-=1
+            return self.Queue.pop(0)
+        else:
+            return False
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.Queue[0]
+
+queue = Queue()
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(4)
+print(queue.is_empty())
+print(queue.peek())
+queue.dequeue()
+print(queue.peek())
+queue.dequeue()
+print(queue.peek())
+queue.dequeue()
+print(queue.peek())
