cc8a0622d5a35eba9be6ec03794bdb5b54f0d056
LoayMohamedAhmed <looay432@gmail.com>
2024-09-19 07:27:48 +0300

    selection sort with linke list

--- Linked list/selectionSort.py
+++ Linked list/selectionSort.py
@@ -7,11 +7,21 @@ class SelectionSort:
         for i in range(linked_list.size-1):
             min_val = current_node.data
             next_node = current_node.next
-            for itr in range(itr,linked_list.size):
+            for itr in range(i,linked_list.size-1):
                 if next_node.data < min_val:
                     min_val = next_node.data
-                    current_node.data = min_val
+                    current_node.data ,next_node.data  = next_node.data , current_node.data
                 next_node = next_node.next
             current_node =current_node.next
-    
+
+linked_list = SLinkedList()
+selSort = SelectionSort()
+linked_list.add(7)
+linked_list.add(2)
+linked_list.add(1)
+linked_list.add(9)
+linked_list.add(5)
+linked_list.add(21)
+selSort.Sort(linked_list)
+linked_list.print()   
                 
